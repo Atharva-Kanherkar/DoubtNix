7c888b587009cec85aeaa8a163f1c2434e1bb043
Atharva-Kanherkar <atharvakanherkar25@gmail.com>
2023-12-22 02:07:44 +0530

    html

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const port = 3000;
 const app = express();
 const connectDB = require("./db");
+const ejs = require( 'ejs' );
+const path  = require('path');
 connectDB();
 const {  validateCookie } = require('./middlewares/auth');
 app.use(express.json());
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
- 
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 app.use("/api/auth", require('./routes/auth'));
 app.use("/api/doubt", require('./routes/doubt.js'));
 app.use("/api/reply", require('./routes/reply.js'));
